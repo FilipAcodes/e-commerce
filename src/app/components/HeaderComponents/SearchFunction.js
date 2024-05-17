eb213fddbbf7bd2d8196f8a1cf04d8c436d0b171
Filip Altankov <filipaltankov@hotmail.com>
2024-05-17 15:39:17 -0400

    basic functionality to search

--- src/app/components/HeaderComponents/SearchFunction.js
+++ src/app/components/HeaderComponents/SearchFunction.js
@@ -1,16 +1,28 @@
+"use client";
 import "./search.css";
 import Image from "../../../../images/magnifying.png";
+import { useState } from "react";
 const SearchFunction = () => {
+  const [userInput, setUserInput] = useState("");
+  const searchUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+  const submitBtn = () => {
+    console.log("Submitted");
+    setUserInput("");
+  };
   return (
     <div className="searchContainer">
       <input
         type="text"
         id="search"
         name="search"
+        value={userInput}
         placeholder="Enter your query"
         className="searchInput"
+        onChange={searchUserInput}
       />
-      <button className="searchBtn">
+      <button className="searchBtn" type="submit" onClick={submitBtn}>
         <img src={Image.src} className="magnifyingIcon"></img>
       </button>
     </div>
