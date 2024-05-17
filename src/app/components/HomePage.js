8f9eed5b22270d2d2f12376e8788e1d14694a0eb
Filip Altankov <filipaltankov@hotmail.com>
2024-05-17 17:10:27 -0400

    LandingPage

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import "./homePage.css";
+import LandingPicture from "../../../images/landingPicture.jpg";
+import DogPaw from "../../../images/paws.png";
 const HomePage = () => {
   return (
-    <div>
-      <div>Picture of a Dog</div>
-      <div>Welcome to DoggoShop</div>
+    <div className="landingPage">
+      <div className="flex1Container">
+        <h1 className="landingh1">Welcome to DoggoShop!</h1>
+        <div className="sherlockBones">
+          <img src={DogPaw.src} alt="dogPaw" className="dogPawImg"></img>
+          <div>
+            <h2 className="inspirational1">Anything is paw-sible.</h2>
+            <h3 className="inspirational2">-Sherlock Bones</h3>
+          </div>
+        </div>
+      </div>
+      <div className="doggoLandingPicture">
+        <img
+          src={LandingPicture.src}
+          alt="picture of dog"
+          className="doggoPic"
+        ></img>
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <h1>Welcome to DoggoShop</h1>
       <HomePage />
       <Footer />
     </main>
