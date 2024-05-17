5bde673a426d82acca7bf4e6e300b242dc16489c
Filip Altankov <filipaltankov@hotmail.com>
2024-05-17 17:29:34 -0400

    icon WIP

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
+"use client";
 import "./globals.css";
-
-export const metadata = {
-  title: "DoggoShop",
-  description: "Project for Vanier college final semester 2024",
-};
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link rel="icon" href="./favicon.ico"></link>
+      </head>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-"use client";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <HomePage />
-      <Footer />
     </main>
   );
 }
+
+export const metadata = {
+  title: "DoggoShop",
+  description: "Project Creater for Vanier College",
+};
